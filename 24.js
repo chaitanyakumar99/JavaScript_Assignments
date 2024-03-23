2ce45551a8037cbebe23be5c9fe0624cfb21085a
USER 1 <gchkumar999@gmail.com>
2024-03-23 23:06:39 +0530

    prat

---
+++ 24.js
@@ -0,0 +1,11 @@
+//24. Write a program to check if a number is 3-digit numbers or not
+const startnum=100;
+const endnum=999;
+
+let num=750;
+if(num>=startnum && num<=endnum){
+    console.log("It is a 3-digit Number");
+}
+else{
+    console.log("It is not a 3-digit Number");
+}
