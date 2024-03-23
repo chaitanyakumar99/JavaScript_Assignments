62160d65fb95f62db9257f615357214e99df66e8
USER 1 <gchkumar999@gmail.com>
2024-03-23 22:01:05 +0530

    prat

---
+++ 23.js
@@ -0,0 +1,9 @@
+//23. Write a program to check if a number is positive or not 
+
+let num=55;
+if(num>30){
+    console.log("Positive Number");
+}
+else{
+    console.log("Not a Positve Number");
+}
