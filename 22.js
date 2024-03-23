a47cfa3accaa3112251a24fe50d0d18a320b7153
USER 1 <gchkumar999@gmail.com>
2024-03-23 21:41:38 +0530

    prat

---
+++ 22.js
@@ -0,0 +1,9 @@
+// 22. Write a program to check if a number is multiple of 3 or not
+
+let num=30;
+if(num%3==0){
+    console.log(" Number is Multiple with 3");
+}
+else{
+    console.log("Number is Not Multiple with 3");
+}
