27200108e7817fd8ed3fdb9bcb1e72d18a43d791
USER 1 <gchkumar999@gmail.com>
2024-03-24 11:51:14 +0530

    prat

---
+++ 26.js
@@ -0,0 +1,9 @@
+//26. Write a program to print the greatest number in given two numbers
+let num1=550;
+let num2=750;
+if(num1<num2){
+    console.log(`${num2} is greater than ${num1}`);
+}
+else{
+    console.log(`${num1} is greater than ${num2}`);
+}
