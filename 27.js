51669571de4e2c44bd5ead53a2a5106fe8cee8f7
USER 1 <gchkumar999@gmail.com>
2024-03-24 12:44:24 +0530

    prat

---
+++ 27.js
@@ -0,0 +1,10 @@
+// 27. Write a program to print the least number given two numbers
+
+let num1=555;
+let num2=850;
+if(num1<num2){
+    console.log(`${num1} is least than ${num2}`);
+}
+else{
+    console.log(`${num2} is least than ${num1}`);
+}
