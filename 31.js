8a7ecda05d1595d44208192b0c150411e5a75663
USER 1 <gchkumar999@gmail.com>
2024-03-28 18:33:49 +0530

    prat

--- 31.js
+++ 31.js
@@ -1,4 +1,5 @@
-//31. Write a program to print the given 3 numbers in descending orders
+//31. Write a program to print the given 3 numbers in descending orders 
+
 let numbers=[25,30,40,50]
 numbers.sort((a,b)=>(b-a)); 
 console.log("Descending order",numbers);
