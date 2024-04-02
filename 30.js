32eb3d64895a320fb680cb1c288436c42da33569
USER 1 <gchkumar999@gmail.com>
2024-04-02 20:07:26 +0530

    prat

--- 30.js
+++ 30.js
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
+
 // 30. Write a program to print the given 3 numbers in ascending orders
 
 let numbers=[10,30,20,40,90,5,100,1]
 numbers.sort((a,b)=>(a-b)); //=>fat arrow This Method Sort the elements of an array in place, meaning it changes the original array
-=======
-// 30. Write a program to print the given 3 numbers in ascending orders
-
-let numbers=[10,30,20,40,90,5,100,1]
-numbers.sort((a,b)=>(a-b)); //=>fat arrow This Method Sort the elements of an array in place, meaning it changes the original array
->>>>>>> eba46e1be84a5fdc13b458636037bf90a3a4e9ad
-console.log("Ascendind order",numbers);
---
+++ 32.js
@@ -0,0 +1,7 @@
+// 32. JavaScript Program to find largest of Three Numbers
+
+function largestNumber(a,b,c,d){
+    return Math.max(a,b,c,d);
+}
+const lgNumber=largestNumber(200,500,100,300);
+console.log(lgNumber);
